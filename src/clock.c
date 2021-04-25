ba41d7eea7b83763b85a66e556f870be4964c02f
Ahmet F. Say <ahmetfazil@gmail.com>
2021-04-25 21:29:44 +0300

    PLL0 : Added multiplier/divider and locked status reading and feed sequence functions.

--- src/clock.c
+++ src/clock.c
@@ -109,5 +109,47 @@ void connect_PLL0(bool connect){
         1 through 32.
  */
 void configure_multiplier_and_predivider_PLL0(unsigned int MSEL0, unsigned int NSEL0){
-    SYSCTL->PLL0CFG = MSEL0 | (NSEL0 << 16);
+    SYSCTL->PLL0CFG = (0x00007FFF & MSEL0) | ((0x000000FF & NSEL0) << 16);
+}
+
+/*
+    14:0 MSEL0
+            Read-back for the PLL0 Multiplier value. This is the value currently
+            used by PLL0, and is one less than the actual multiplier
+*/
+unsigned int multipler_value_PLL0(void){
+    return SYSCTL->PLL0STAT & 0x00007FFF;
+}
+
+/*
+    23:16 NSEL0
+        Read-back for the PLL0 Pre-Divider value. This is the value
+        currently used by PLL0, and is one less than the actual divider.
+*/
+unsigned int predivider_value_PLL0(void){
+    return ((SYSCTL->PLL0STAT)>>16) & 0x000000FF;
+}
+
+/*
+    26 PLOCK0
+        Reflects the PLL0 Lock status. When zero, PLL0 is not locked.
+        When one, PLL0 is locked onto the requested frequency. See text
+        for details.
+*/
+bool is_locked_PLL0(void){
+    return (SYSCTL->PLL0STAT & PLL0_LOCKED_MASK)?true:false; 
+}
+/*
+    7:0 PLL0FEED 
+        The PLL0 feed sequence must be written to this register in order for
+        PLL0 configuration and control register changes to take effect
+
+        The two writes must be in the correct sequence, and there must be no other register access in the same address space (0x400F C000 to 0x400F FFFF) between them.
+        Because of this, it may be necessary to disable interrupts for the duration of the PLL0 feed operation, if there is a possibility that an interrupt service routine could write to another
+        register in that space. If either of the feed values is incorrect, or one of the previously mentioned conditions is not met, any changes to the PLL0CON or PLL0CFG register will
+        not become effective.
+*/
+void feed_sequence_PLL0(void){
+    SYSCTL->PLL0FEED = PLL0FEED_1;
+    SYSCTL->PLL0FEED = PLL0FEED_2;
 }
